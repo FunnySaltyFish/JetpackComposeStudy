937549f9d158d7eedf00ec9a6fec5346f3406bb4
FunnySaltyFish <3036548996@qq.com>
2023-06-20 18:45:54 +0800

    Pager 的 Tab 可点击

--- app/src/main/java/com/funny/compose/study/ui/feature/Pager1_4.kt
+++ app/src/main/java/com/funny/compose/study/ui/feature/Pager1_4.kt
@@ -1,7 +1,11 @@
 package com.funny.compose.study.ui.feature
 
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -11,11 +15,13 @@ import androidx.compose.material.TabRow
 import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.pagerTabIndicatorOffset
+import kotlinx.coroutines.launch
 
 private val pages = listOf("生活", "美食", "科技", "娱乐", "涩涩")
 
@@ -23,38 +29,51 @@ private val pages = listOf("生活", "美食", "科技", "娱乐", "涩涩")
 @Composable
 fun HorizontalPagerWithIndicator() {
     val pagerState = rememberPagerState()
+    val scope = rememberCoroutineScope()
 
-    TabRow(
-        // Our selected tab is our current page
-        selectedTabIndex = pagerState.currentPage,
-        // Override the indicator, using the provided pagerTabIndicatorOffset modifier
-        indicator = { tabPositions ->
-            TabRowDefaults.Indicator(
-                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+    Column {
+        TabRow(
+            modifier = Modifier.fillMaxWidth(),
+            // Our selected tab is our current page
+            selectedTabIndex = pagerState.currentPage,
+            // Override the indicator, using the provided pagerTabIndicatorOffset modifier
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(
+                        pagerState = pagerState,
+                        tabPositions = tabPositions
+                    )
+                )
+            }
+        ) {
+            // Add tabs for all of our pages
+            pages.forEachIndexed { index, title ->
+                Tab(
+                    text = { Text(title) },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        Log.d("PagerTest", "HorizontalPagerWithIndicator: onClicked")
+                        scope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                )
+            }
+        }
+
+        HorizontalPager(
+            pageCount = pages.size,
+            state = pagerState,
+            modifier = Modifier.fillMaxSize().background(MaterialTheme.colors.surface)
+        ) { page ->
+            Text(
+                text = "Page $page",
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+                textAlign = TextAlign.Center,
             )
         }
-    ) {
-        // Add tabs for all of our pages
-        pages.forEachIndexed { index, title ->
-            Tab(
-                text = { Text(title) },
-                selected = pagerState.currentPage == index,
-                onClick = { /* TODO */ },
-            )
-        }
-    }
-
-    HorizontalPager(
-        pageCount = pages.size,
-        state = pagerState,
-    ) { page ->
-        Text(
-            text = "Page $page",
-            style = MaterialTheme.typography.h5,
-            modifier = Modifier
-                .fillMaxSize()
-                .wrapContentSize(Alignment.Center),
-            textAlign = TextAlign.Center,
-        )
     }
 }
