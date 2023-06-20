15797b995933d84af60a3cd4317ddbde5238e32a
FunnySaltyFish <3036548996@qq.com>
2023-06-20 18:45:38 +0800

    优化页面效果

--- app/src/main/java/com/funny/compose/study/ui/anim/NumberChangeAnimation.kt
+++ app/src/main/java/com/funny/compose/study/ui/anim/NumberChangeAnimation.kt
@@ -2,8 +2,6 @@ package com.funny.compose.study.ui.anim
 
 import androidx.compose.animation.*
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
@@ -18,78 +16,50 @@ import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
 import kotlin.math.roundToInt
 
-@OptIn(ExperimentalAnimationApi::class)
-@Composable
-fun NumberChangeAnimationText(
-    modifier: Modifier = Modifier,
-    text: String,
-    textPadding: PaddingValues = PaddingValues(horizontal = 8.dp, vertical = 12.dp),
-    textSize: TextUnit = 24.sp,
-    textColor: Color = Color.Black
-) {
-    Row(modifier = modifier) {
-        text.forEach {
-            AnimatedContent(
-                targetState = it,
-                transitionSpec = {
-                    slideIntoContainer(AnimatedContentScope.SlideDirection.Up) with
-                            fadeOut() + slideOutOfContainer(AnimatedContentScope.SlideDirection.Up)
-                }
-            ) { char ->
-                Text(text = char.toString(), modifier = modifier.padding(textPadding), fontSize = textSize, color = textColor)
-            }
-        }
-    }
-}
-
 @Composable
 fun NumberChangeAnimationTextTest() {
-//    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-//        var text by remember { mutableStateOf("103") }
-//        NumberChangeAnimationText(text = text)
-//
-//        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-//            // 加一 和 减一
-//            listOf(1, -1).forEach { i ->
-//                TextButton(onClick = {
-//                    text = (text.toInt() + i).toString()
-//                }) {
-//                    Text(text = if (i == 1) "加一" else "减一")
-//                }
-//            }
-//        }
-//    }
-    val text =  remember {
-        mutableStateOf("————————————")
-    }
-    Column(modifier = Modifier.fillMaxSize()){
-        Box(modifier = Modifier
-            .height(100.dp)
-            .clickable {
-                text.value = System.currentTimeMillis().toString()
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        var text by remember { mutableStateOf("103") }
+        NumberChangeAnimatedText(text = text)
+
+        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+            // 加一 和 减一
+            listOf(1, -1).forEach { i ->
+                TextButton(onClick = {
+                    text = (text.toInt() + i).toString()
+                }) {
+                    Text(text = if (i == 1) "加一" else "减一")
+                }
             }
-            .background(Color.Red), contentAlignment = Alignment.Center){
-            Text("测试文本")
         }
-        Box(modifier = Modifier
-            .height(100.dp)
-            .fillMaxWidth()
-            .background(Color.Red), contentAlignment = Alignment.Center){
-            NumberChangeAnimatedText(text = text.value)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        val longText = remember {
+            mutableStateOf("————————————")
         }
 
-        Box(modifier = Modifier
-            .height(100.dp)
-            .fillMaxWidth()
-            .background(Color.Red), contentAlignment = Alignment.Center){
+        LaunchedEffect(key1 = Unit, block = {
+            delay(2000)
+            longText.value = "这是测试动画"
+        })
+
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .fillMaxWidth(), contentAlignment = Alignment.Center
+        ) {
+            NumberChangeAnimatedText(text = longText.value)
+        }
+
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .fillMaxWidth(), contentAlignment = Alignment.Center
+        ) {
             AutoIncreaseAnimatedNumber(number = 10000, durationMills = 11000)
         }
     }
 
-    LaunchedEffect(key1 = Unit, block = {
-        delay(2000)
-        text.value = "这是测试动画"
-    })
 }
 
 @OptIn(ExperimentalAnimationApi::class)
@@ -111,7 +81,13 @@ fun NumberChangeAnimatedText(
                             fadeOut() + slideOutOfContainer(AnimatedContentScope.SlideDirection.Up)
                 }
             ) { char ->
-                Text(text = char.toString(), modifier = modifier.padding(textPadding), fontSize = textSize, color = textColor, fontWeight = textWeight)
+                Text(
+                    text = char.toString(),
+                    modifier = modifier.padding(textPadding),
+                    fontSize = textSize,
+                    color = textColor,
+                    fontWeight = textWeight
+                )
             }
         }
     }
